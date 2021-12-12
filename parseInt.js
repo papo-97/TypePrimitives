a8742abd219cdcd7e54b961ce8f4ac0cdb4895ec
papo-97 <95270166+papo-97@users.noreply.github.com>
2021-12-12 10:23:24 -0800

    Write logic that implements parseInt

---
+++ parseInt.js
@@ -0,0 +1,16 @@
+function parsInt(str) {
+  let output = "";
+  for (let j = 0; j < str.length; j++) {
+    if (str[j] >= 0 || str[j] < 0) {
+      output += str[j];
+    } else {
+      break;
+    }
+  }
+  if(output.length===0){
+      return "is not a number";
+  }
+  else {
+      return  +output;
+  }
+}
